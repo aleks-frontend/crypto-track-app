97f011894c8e4ee2701a3bfb199f311553278b07
Aleksandar Gojkovic <aleksfrontend@gmail.com>
2021-02-25 10:38:08 +0100

    chore: set up helpers for globally shared data

---
+++ src/helpers.js
@@ -0,0 +1,21 @@
+export const apiKey = '241249c5-fd26-445f-80a0-812e66445a7b';
+export const baseEndpointURL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency';
+
+export const fontSizes = {
+    regular: '0.8rem'
+}
+
+export const colors = {
+    silverChalice: '#aaaaaa',    
+    doveGray: '#616161'
+}
+
+export const cssSnippets = {
+    flexColumnCenter: `display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;`,
+    flexRowCenter: `display: flex;
+    align-items: center;
+    justify-content: center;`,
+}
