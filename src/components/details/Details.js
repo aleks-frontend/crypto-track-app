66077682a300c0005696c86a45408fd2ab344c6f
Aleksandar Gojkovic <aleksfrontend@gmail.com>
2021-02-28 17:10:30 +0100

    refactor: updated Details to use Hooks

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -19,86 +19,91 @@ const DetailsItem = styled.div`
     font-size: ${fontSizes.regular};
 `;
 
-class Details extends React.Component {
-    constructor(props) {
-        super(props);
+const Details = props => {
+    const isMountedRef = React.useRef(false);
+    const [currencyData, setCurrencyData] = React.useState({
+        name: '',
+        cmcRank: '',
+        symbol: '',
+        price: '',
+        totalSupply: ''
+    });
+    const [loaded, setLoaded] = React.useState(false);
 
-        this.state = {
-            name: '',
-            cmcRank: '',
-            symbol: '',
-            price: '',
-            totalSupply: '',
-            loaded: false
-        };
+    // Helper function for routing back to homepage
+    const navigateToHome = () => {
+        props.history.push(`/`);
+    };
 
-        this.fetchInterval = null;
-
-        this.navigateToHome = this.navigateToHome.bind(this);
-        this.fetchData = this.fetchData.bind(this);
-    }
-
-    fetchData() {
-        const { currencyId } = this.props.match.params;
+    // Helper function for fetching the data
+    const fetchData = () => {
+        const { currencyId } = props.match.params;
         const endpointURL = `${baseEndpointURL}/quotes/latest?id=${currencyId}&CMC_PRO_API_KEY=${apiKey}`;
 
         fetch(endpointURL)
             .then(response => response.json())
             .then(result => {
-                const currencyDetails = result.data[currencyId];
-                const { name, cmc_rank: cmcRank, symbol, total_supply: totalSupply } = currencyDetails;
-                const { price } = currencyDetails.quote.USD;
+                // Checking if the component got unmounted while the data was being fetched
+                if (isMountedRef.current) {
+                    const currencyDetails = result.data[currencyId];
+                    const { name, cmc_rank: cmcRank, symbol, total_supply: totalSupply } = currencyDetails;
+                    const { price } = currencyDetails.quote.USD;
 
-                this.setState({
-                    name,
-                    cmcRank,
-                    symbol,
-                    price,
-                    totalSupply,
-                    loaded: true
-                });
+                    // Setting the fetched data to 'currencyData' state
+                    setCurrencyData(prevData => ({
+                        ...prevData,
+                        name,
+                        cmcRank,
+                        symbol,
+                        price,
+                        totalSupply
+                    }));
+
+                    // Setting the 'loaded' state to 'true' after the data is fetched
+                    setLoaded(true);
+                }
             })
             .catch(err => {
                 console.error(err);
             });
-    }
+    };
 
-    componentDidMount() {
-        this.fetchData();
-        this.fetchInterval = setInterval(() => {
-            this.fetchData();
-        }, 60000)
-    }
+    React.useEffect(() => {
+        isMountedRef.current = true;
+        fetchData();
 
-    componentWillUnmount() {
-        clearInterval(this.fetchInterval);
-    }
+        // Fetching the data every 60 seconds
+        let fetchInterval = setInterval(() => {
+            fetchData();
+        }, 60000);
 
-    navigateToHome() {
-        this.props.history.push(`/`);
-    }
+        // Clearing the interval to prevent the momory leak once the component was unmounted
+        return () => {
+            isMountedRef.current = false;
+            clearInterval(fetchInterval);
+        };
+    }, []);
 
-    render(props) {
-        const { name, cmcRank, symbol, price, totalSupply } = this.state;
+    // Using destructuring to get all the values from the currency data
+    const { name, cmcRank, symbol, price, totalSupply } = currencyData;
 
-        return (
-            <>
-                <DetailsMain>
-                    {this.state.loaded ?
-                        <>
-                            <DetailsHeading>{name}</DetailsHeading>
-                            <DetailsItem><strong>Symbol:</strong> {symbol}</DetailsItem>
-                            <DetailsItem><strong>CMC Rank:</strong> {cmcRank}</DetailsItem>
-                            <DetailsItem><strong>Current Price:</strong> ${price.toFixed(2)}</DetailsItem>
-                            <DetailsItem><strong>Total Supply:</strong> ${totalSupply}</DetailsItem>
-                            <GenericButton onClick={this.navigateToHome}>Back to Homepage</GenericButton>
-                        </>
-                        : <LoadingSpinner />
-                    }
-                </DetailsMain>
-            </>
-        )
-    }
-}
+    return (
+        <>
+            <DetailsMain>
+                {loaded ?
+                    <>
+                        <DetailsHeading>{name}</DetailsHeading>
+                        <DetailsItem><strong>Symbol:</strong> {symbol}</DetailsItem>
+                        <DetailsItem><strong>CMC Rank:</strong> {cmcRank}</DetailsItem>
+                        <DetailsItem><strong>Current Price:</strong> ${price.toFixed(2)}</DetailsItem>
+                        <DetailsItem><strong>Total Supply:</strong> ${totalSupply}</DetailsItem>
+                        <GenericButton onClick={navigateToHome}>Back to Homepage</GenericButton>
+                    </>
+                    : <LoadingSpinner />
+                }
+            </DetailsMain>
+        </>
+    )
+};
 
 export default Details;
